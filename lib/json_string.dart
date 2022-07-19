16f00db875b856d2d8d34a4be93fc81518dbbe24
fulmoon <63829420+fulmoon@users.noreply.github.com>
2022-07-19 17:34:32 +0900

    Feat: add json to map, progrss indicator onMini

--- lib/json_string.dart
+++ lib/json_string.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 class JsonString extends StatefulWidget {
@@ -8,18 +10,47 @@ class JsonString extends StatefulWidget {
 }
 
 class _JsonStringState extends State<JsonString> {
+
+  Map<String, dynamic>? person;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    initData();
+  }
+
+  Future initData() async{
+    person = await getData();
+
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(''),
+        title: const Text('Searching App'),
       ), //AppBar
       body: Center(
-        child: Text(
-          "결과가 나올 부분",
-          style: TextStyle(fontSize: 40),
+        child: person == null
+        ? const CircularProgressIndicator()
+        : Text(
+          "이름은 ${person!['name']} 이고, 이메일은 ${person!['email']} 입니다.",
+          style: const TextStyle(fontSize: 30),
         ),
       ),
     );
+  }
+
+  Future<Map<String, dynamic>> getData() async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    String jsonString = '{"name": "홍길동", "email": "aaa@aaa.com"}';
+
+    Map<String, dynamic> jsonMap = jsonDecode(jsonString);
+
+    return jsonMap;
   }
 }
