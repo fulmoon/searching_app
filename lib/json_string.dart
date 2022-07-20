2e61ec2de9dd1f57bb2deb1daba04875540e8af5
fulmoon <fulmoon@gmail.com>
2022-07-20 18:36:31 +0900

    Refact: adapt modelclass onAir

--- lib/json_string.dart
+++ lib/json_string.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'data.dart';
+import 'model/picture.dart';
+import 'model/images.dart';
 
 class JsonString extends StatefulWidget {
   const JsonString({Key? key}) : super(key: key);
@@ -9,9 +12,8 @@ class JsonString extends StatefulWidget {
 }
 
 class _JsonStringState extends State<JsonString> {
-
   TextEditingController _controller = TextEditingController();
-  List<Map<String, dynamic>> hits= [];
+  List<Picture> _images = [];
 
   @override
   void initState() {
@@ -21,7 +23,7 @@ class _JsonStringState extends State<JsonString> {
   }
 
   @override
-  void didChangeDependencies(){
+  void didChangeDependencies() {
     super.didChangeDependencies();
 
     initData();
@@ -34,7 +36,7 @@ class _JsonStringState extends State<JsonString> {
   }
 
   Future initData() async {
-    hits = await getHits();
+    _images = await getImages();
 
     setState(() {});
   }
@@ -42,103 +44,94 @@ class _JsonStringState extends State<JsonString> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: const Text(
-            '이미지 검색 앱',
-            style: TextStyle(color: Colors.black),
-          ),
-          backgroundColor: Colors.transparent,
-          elevation: 0.0,
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          '이미지 검색 앱',
+          style: TextStyle(color: Colors.black),
         ),
-        body: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),       //Appbar와 겹쳐 보이는 문제 발생.
-              child: TextField(       //샘플코드
-                controller: _controller,
-                onSubmitted: (String value) async {
-                  await showDialog<void>(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        title: const Text('Thanks!'),
-                        content: Text(
-                            'You typed "$value", which has length ${value.characters.length}.'),
-                        actions: <Widget>[
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pop(context);
-                              //const CircularProgressIndicator();
-                            },
-                            child: const Text('OK'),
-                          ),
-                        ],
-                      );
-                    },
-                  );
-                },
-                obscureText: true,
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(),
-                  //border color 적용 안됨
-                  //border: OutlineInputBorder(borderSide: Color(0xFF0000FF)),
-                  labelText: 'Search',
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                      });
-                    },
-                    icon: const Icon(Icons.search),
-                  ),
+        backgroundColor: Colors.transparent,
+        elevation: 0.0,
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            //Appbar와 겹쳐 보이는 문제 발생.
+            child: TextField(
+              //샘플코드
+              controller: _controller,
+              onSubmitted: (String value) async {
+                await showDialog<void>(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: const Text('Thanks!'),
+                      content: Text(
+                          'You typed "$value", which has length ${value.characters.length}.'),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                            //const CircularProgressIndicator();
+                          },
+                          child: const Text('OK'),
+                        ),
+                      ],
+                    );
+                  },
+                );
+              },
+              obscureText: true,
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                //border color 적용 안됨
+                //border: OutlineInputBorder(borderSide: Color(0xFF0000FF)),
+                labelText: 'Search',
+                suffixIcon: IconButton(
+                  onPressed: () {
+                    setState(() {});
+                  },
+                  icon: const Icon(Icons.search),
                 ),
               ),
             ),
-           Expanded(
-             child: GridView.builder(
-                gridDelegate:
-                    const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 1,
-                    ),
-                itemCount: hits.length,
-                //itemCount: 50, //임의로 테스트
-                itemBuilder: (BuildContext context, index) {
-                  Map<String, dynamic> image = hits[index];
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(30),
-                      child: Image.network(
-                        image['previewURL'],
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  );
-                },
+          ),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 1,
               ),
-           ),
-          ],
-        )
-
-        /*Expanded(
-            child:
-            ListView.builder(
-              itemBuilder: (BuildContext context, int index) {
-                Map<String, dynamic> image = images![index];
-
-                return Image.network(image['previewURL']);
+              itemCount: _images.length,
+              //itemCount: 50, //임의로 테스트
+              itemBuilder: (BuildContext context, index) {
+                Picture image = _images[index];
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: Image.network(
+                      image.previewURL,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                );
               },
-            ),*/
-        );
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
-  Future<List<Map<String, dynamic>>> getHits() async {
+  Future<List<Picture>> getImages() async {
     await Future.delayed(const Duration(seconds: 2));
-    List<Map<String, dynamic>> hits = data['hits'];
 
-    return hits;
+    String jsonString = images;
+
+    Map<String, dynamic> json = jsonDecode(jsonString);
+    Iterable hits = json['hits'];
+    return hits.map((e) => Picture.fromJson(e)).toList();
   }
-
-  //Future<>
 }
