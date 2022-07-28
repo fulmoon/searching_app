2cc1b7e13dacc0c772bf5d5405cfc621a7be03fb
fulmoon <fulmoon@gmail.com>
2022-07-28 23:58:29 +0900

    Fix: change picture_id to String  onAir

--- lib/model/video_data.dart
+++ lib/model/video_data.dart
@@ -1,6 +1,6 @@
 class VideoData {
   final int id;
-  final int picture_id;
+  final String picture_id;
   final String tags;
 
   VideoData({
@@ -12,7 +12,7 @@ class VideoData {
   factory VideoData.fromJson(Map<String, dynamic> json) {
     return VideoData(
       id: json['id'] as int,
-      picture_id: json['picture_id'] as int,
+      picture_id: json['picture_id'] as String,
       tags: json['tags'] as String,
     );
   }
