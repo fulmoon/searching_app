4723e76553b127bc30a0927d41ba5cc4dfe9bd6e
fulmoon <63829420+fulmoon@users.noreply.github.com>
2022-07-31 10:05:46 +0900

    Feat: add counterapp using Stream listener  onMini

--- lib/counter_stream/counter_stream_app.dart
+++ lib/counter_stream/counter_stream_app.dart
@@ -1,10 +1,40 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:searching_app/counter_stream/counter_api.dart';
 
-class CounterStreamApp extends StatelessWidget {
-  CounterStreamApp({Key? key}) : super(key: key);
+class CounterStreamApp extends StatefulWidget {
+  const CounterStreamApp({Key? key}) : super(key: key);
 
+  @override
+  State<CounterStreamApp> createState() => _CounterStreamAppState();
+}
+
+class _CounterStreamAppState extends State<CounterStreamApp> {
   final _api = CounterApi();
+
+  int _count = 0;
+
+  StreamSubscription<int>? _subscriptions;
+
+  @override
+  void initState() {
+    super.initState();
+    _subscriptions = _api.countStream.listen((count) {
+      setState(() {
+        _count = count;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // if(_subscriptions != null){
+    //   _subscriptions!.cancel();
+    // }
+    _subscriptions?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,15 +43,9 @@ class CounterStreamApp extends StatelessWidget {
         title: const Text('카운터 Stream 버전'),
       ), //AppBar
       body: Center(
-        child: StreamBuilder<int>(
-          initialData: 0,
-          stream: _api.countStream,
-          builder: (context, snapshot) {
-            return Text(
-              "${snapshot.data}",
+        child: Text(
+              "$_count",
               style: Theme.of(context).textTheme.headline1,
-            );
-          }
         ),
       ),
       floatingActionButton: FloatingActionButton(
